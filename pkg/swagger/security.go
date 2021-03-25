fda1236018e286d0009a3cdde5d9a63694ad7ca5
Tim Shi <tishi@cisco.com>
2021-03-25 15:48:47 -0400

    updated swagger access rule

--- pkg/swagger/security.go
+++ pkg/swagger/security.go
@@ -4,8 +4,10 @@ import (
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/security"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/security/access"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/security/errorhandling"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/security/oauth2"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/security/oauth2/tokenauth"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/web/matcher"
+	"errors"
 )
 
 type swaggerSecurityConfigurer struct {}
@@ -23,7 +25,19 @@ func (c *swaggerSecurityConfigurer) Configure(ws security.WebSecurity) {
 }
 
 func swaggerSpecAccessControl(auth security.Authentication) (decision bool, reason error) {
-	//TODO: should differentiate between user auth and client auth,
-	// should also check oauth2 scope?
+	oa, ok := auth.(oauth2.Authentication)
+	if !ok {
+		return false, errors.New("expected oauth authentication")
+	}
+
+	if oa.UserAuthentication() == nil {
+		return false, errors.New("expected oauth user authentication")
+	}
+
+	if !(oa.OAuth2Request().Scopes().Has("read") && oa.OAuth2Request().Scopes().Has("write")) {
+		return false, errors.New("expected read and write scope")
+	}
+
+	//and must be authenticated
 	return access.Authenticated(auth)
 }
