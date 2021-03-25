2874c8894e185a1f2817429dc06882a20a99f98d
Tim Shi <tishi@cisco.com>
2021-03-25 15:53:25 -0400

    fix error type

--- pkg/swagger/security.go
+++ pkg/swagger/security.go
@@ -7,7 +7,6 @@ import (
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/security/oauth2"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/security/oauth2/tokenauth"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/web/matcher"
-	"errors"
 )
 
 type swaggerSecurityConfigurer struct {}
@@ -27,15 +26,15 @@ func (c *swaggerSecurityConfigurer) Configure(ws security.WebSecurity) {
 func swaggerSpecAccessControl(auth security.Authentication) (decision bool, reason error) {
 	oa, ok := auth.(oauth2.Authentication)
 	if !ok {
-		return false, errors.New("expected oauth authentication")
+		return false, security.NewInsufficientAuthError("expected token authentication")
 	}
 
 	if oa.UserAuthentication() == nil {
-		return false, errors.New("expected oauth user authentication")
+		return false, security.NewInsufficientAuthError("expected oauth user authentication")
 	}
 
-	if !(oa.OAuth2Request().Scopes().Has("read") && oa.OAuth2Request().Scopes().Has("write")) {
-		return false, errors.New("expected read and write scope")
+	if !(oa.OAuth2Request().Approved() && oa.OAuth2Request().Scopes().Has("read") && oa.OAuth2Request().Scopes().Has("write")) {
+		return false, security.NewInsufficientAuthError("expected read and write scope")
 	}
 
 	//and must be authenticated
