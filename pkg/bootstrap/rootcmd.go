e69dbf742ce407a580e57a293e4055d636ee7bfc
Tim Shi <tishi@cisco.com>
2021-06-24 17:12:58 -0400

    changed a typo

--- pkg/bootstrap/rootcmd.go
+++ pkg/bootstrap/rootcmd.go
@@ -63,7 +63,7 @@ func init() {
 	// config flags
 	rootCmd.PersistentFlags().StringSliceVarP(&cliCtx.ActiveProfiles, CliFlagActiveProfile, "P", []string{},
 		`Comma separated active profiles. Override property "application.profiles.active"`)
-	rootCmd.PersistentFlags().StringSliceVarP(&cliCtx.AdditionalProfiles, CliFlagAdditionalProfile, "a", []string{},
+	rootCmd.PersistentFlags().StringSliceVarP(&cliCtx.AdditionalProfiles, CliFlagAdditionalProfile, "p", []string{},
 		`Comma separated additional profiles. Set property "application.profiles.additional". Additional profiles is added to active profiles`)
 	rootCmd.PersistentFlags().StringSliceVarP(&cliCtx.ConfigSearchPaths, CliFlagConfigSearchPath, "c", []string{},
 		`Comma separated paths. Override property "config.file.search-path"`)
