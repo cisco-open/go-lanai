c3b626970cd717db9e848c658481f9c8208a38dd
Tim Shi <tishi@cisco.com>
2021-06-30 17:24:02 -0400

    added BeforeSave hook to do tenancy validation and calculation

--- pkg/data/types/gorm_model.go
+++ pkg/data/types/gorm_model.go
@@ -2,6 +2,10 @@ package types
 
 import (
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/data/types/pqx"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/security"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/tenancy"
+	"errors"
+	"fmt"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
@@ -21,4 +25,25 @@ type SoftDelete struct {
 type Tenancy struct {
 	TenantID   uuid.UUID     `gorm:"type:UUID;not null"`
 	TenantPath pqx.UUIDArray `gorm:"type:uuid[];index:,type:gin;not null"  json:"-"`
+}
+
+func (t *Tenancy) BeforeSave(tx *gorm.DB) error {
+	//if tenantId is not available
+	if t.TenantID == uuid.Nil {
+		return errors.New("tenantId is required")
+	}
+
+	if !security.HasAccessToTenant(tx.Statement.Context, t.TenantID.String()) {
+		return errors.New(fmt.Sprintf("user does not have access to tenant %s", t.TenantID.String()))
+	}
+
+	path, err := tenancy.GetTenancyPath(tx.Statement.Context, t.TenantID.String())
+
+	if err != nil {
+		return err
+	}
+
+	t.TenantPath = path
+
+	return nil
 }
