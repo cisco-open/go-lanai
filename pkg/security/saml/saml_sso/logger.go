909518c8def94a5d3b766b5e1e2916f994239fb4
Tim Shi <tishi@cisco.com>
2021-08-09 17:05:45 -0400

    updated saml logger methods

--- pkg/security/saml/saml_sso/logger.go
+++ pkg/security/saml/saml_sso/logger.go
@@ -21,15 +21,15 @@ func (s *loggerAdapter) Printf(format string, v ...interface{}) {
 }
 
 func (s *loggerAdapter) Print(v ...interface{}) {
-	s.delegate.Info("", v...)
+	s.delegate.Info(fmt.Sprint(v...))
 }
 
 func (s *loggerAdapter) Println(v ...interface{}) {
-	s.delegate.Info("", v...)
+	s.delegate.Info(fmt.Sprint(v...))
 }
 
 func (s *loggerAdapter) Fatal(v ...interface{}) {
-	s.delegate.Error("", v...)
+	s.delegate.Error(fmt.Sprint(v...))
 	os.Exit(1)
 }
 
@@ -39,12 +39,12 @@ func (s *loggerAdapter) Fatalf(format string, v ...interface{}) {
 }
 
 func (s *loggerAdapter) Fatalln(v ...interface{}) {
-	s.delegate.Error("", v...)
+	s.delegate.Error(fmt.Sprint(v...))
 	os.Exit(1)
 }
 
 func (s *loggerAdapter) Panic(v ...interface{}) {
-	s.delegate.Error("", v...)
+	s.delegate.Error(fmt.Sprint(v...))
 	panic(fmt.Sprint(v...))
 }
 
@@ -54,6 +54,6 @@ func (s *loggerAdapter) Panicf(format string, v ...interface{}) {
 }
 
 func (s *loggerAdapter) Panicln(v ...interface{}) {
-	s.delegate.Error("", v...)
+	s.delegate.Error(fmt.Sprint(v...))
 	panic(fmt.Sprint(v...))
 }
