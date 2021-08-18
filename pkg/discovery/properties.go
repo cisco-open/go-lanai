bd244229eb9d66a4c17116356e3006ceac529b27
Livan Du <livan@kiimobiletech.com>
2021-08-18 14:46:27 -0400

    parsing string into map and slice when read flat properties

--- pkg/discovery/properties.go
+++ pkg/discovery/properties.go
@@ -2,35 +2,37 @@ package discovery
 
 import (
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/bootstrap"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/utils"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/web"
 	"fmt"
 	"github.com/pkg/errors"
 )
 
+//goland:noinspection GoNameStartsWithPackageName
 const (
 	applicationPropertiesPrefix = "application"
 	DiscoveryPropertiesPrefix   = "cloud.consul.discovery"
 )
 
+//goland:noinspection GoNameStartsWithPackageName
 type DiscoveryProperties struct {
-	HealthCheckPath            string   `json:"health-check-path"`
-	HealthCheckInterval        string   `json:"health-check-interval"`
-	Tags                       []string `json:"tags"`
-	AclToken                   string   `json:"acl-token"`
-	IpAddress                  string   `json:"ip-address"` //A pre-defined IP address
-	Interface                  string   `json:"interface"`    //The network interface from where to get the ip address. If IpAddress is defined, this field is ignored
-	Port                       int      `json:"port"`
-	Scheme                     string   `json:"scheme"`
-	HealthCheckCriticalTimeout string   `json:"health-check-critical-timeout"` //See api.AgentServiceCheck's DeregisterCriticalServiceAfter field
-	//TODO: add other values if needed
+	HealthCheckPath            string                    `json:"health-check-path"`
+	HealthCheckInterval        string                    `json:"health-check-interval"`
+	Tags                       utils.CommaSeparatedSlice `json:"tags"`
+	AclToken                   string                    `json:"acl-token"`
+	IpAddress                  string                    `json:"ip-address"` //A pre-defined IP address
+	Interface                  string                    `json:"interface"`  //The network interface from where to get the ip address. If IpAddress is defined, this field is ignored
+	Port                       int                       `json:"port"`
+	Scheme                     string                    `json:"scheme"`
+	HealthCheckCriticalTimeout string                    `json:"health-check-critical-timeout"` //See api.AgentServiceCheck's DeregisterCriticalServiceAfter field
 }
 
 func NewDiscoveryProperties(serverProps *web.ServerProperties) *DiscoveryProperties {
 	return &DiscoveryProperties{
-		Port: serverProps.Port,
-		Scheme: "http",
+		Port:                serverProps.Port,
+		Scheme:              "http",
 		HealthCheckInterval: "15s",
-		HealthCheckPath: fmt.Sprintf("%s%s", serverProps.ContextPath, "/admin/health"),
+		HealthCheckPath:     fmt.Sprintf("%s%s", serverProps.ContextPath, "/admin/health"),
 	}
 }
 
