cba854d3b2e6934b9957ca3dcb316d905fcb73f0
xinzlu <xinzlu@cisco.com>
2022-03-25 14:14:48 -0700

    DE14345 migrations failed with non root user

--- pkg/data/cockroach/db_creator.go
+++ pkg/data/cockroach/db_creator.go
@@ -8,16 +8,23 @@ import (
 )
 
 type GormDbCreator struct {
+	dbUser string
 	dbName string
 }
 
 func NewGormDbCreator(properties CockroachProperties) data.DbCreator {
 	return &GormDbCreator{
+		dbUser: properties.Username,
 		dbName: properties.Database,
 	}
 }
 
 func (g *GormDbCreator) CreateDatabaseIfNotExist(ctx context.Context, db *gorm.DB) error {
+	if g.dbUser != "root" {
+		logger.WithContext(ctx).Info("db user is not a privileged account, skipped db creation.")
+		return nil
+	}
 	result := db.WithContext(ctx).Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", g.dbName))
 	return result.Error
+
 }
