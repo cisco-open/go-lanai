3bf65037b5d99eff63cd770da579a325e4ea2010
Tim Shi <tishi@cisco.com>
2022-10-20 17:08:31 -0400

    added more comprehensive validation for web.Fx*Providers

--- pkg/web/fx_test.go
+++ pkg/web/fx_test.go
@@ -2,14 +2,16 @@ package web
 
 import (
 	"context"
-	"github.com/google/uuid"
+	"errors"
 	"go.uber.org/fx"
 	"net/http"
+	"reflect"
 	"testing"
 )
+import . "github.com/onsi/gomega"
 
 type TestController struct {
-	ID uuid.UUID
+	Description string
 }
 
 func NewTestController() *TestController {
@@ -113,5 +115,131 @@ func TestFxControllerProviderWorksForInterface(t *testing.T) {
 
 	if err != nil {
 		t.Error("fx failed to stop")
+	}
+}
+
+func TestFxControllerProviderWorksForMultipleReturnValues(t *testing.T) {
+	cIface := Controller(&TestController{Description: "Provided as Controller"})
+	cPtr := &TestController{Description: "Provided as *TestController"}
+	notCptr := &NotController{}
+
+	tests := []struct {
+		name                string
+		target              interface{}
+		expectedControllers []interface{}
+		expectedPanic       bool
+		expectedFxError     bool
+	}{
+		{
+			name: "target provides multiple controllers",
+			target: func() (Controller, *TestController, error) {
+				return cIface, cPtr, nil
+			},
+			expectedControllers: []interface{}{cIface, cPtr},
+			expectedPanic:       false,
+			expectedFxError:     false,
+		},
+		{
+			name: "target provides none controller",
+			target: func() (Controller, *TestController, *NotController, error) {
+				return cIface, cPtr, notCptr, nil
+			},
+			expectedControllers: nil,
+			expectedPanic:       true,
+			expectedFxError:     false,
+		},
+		{
+			name: "target provides no controller",
+			target: func() error {
+				return nil
+			},
+			expectedControllers: nil,
+			expectedPanic:       true,
+			expectedFxError:     false,
+		},
+		{
+			name: "target provides result in error",
+			target: func() (Controller, error) {
+				return nil, errors.New("error providing controller")
+			},
+			expectedControllers: nil,
+			expectedPanic:       false,
+			expectedFxError:     true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewWithT(t)
+			defer func() {
+				r := recover()
+				g.Expect(r != nil).To(Equal(tt.expectedPanic))
+			}()
+
+			app := fx.New(
+				FxControllerProviders(tt.target),
+				fx.Invoke(func(di controllerDI) {
+					g.Expect(len(di.Controllers)).To(Equal(len(tt.expectedControllers)))
+					for i := 0; i < len(tt.expectedControllers); i++ {
+						found := false
+						for j := 0; j < len(di.Controllers); j++ {
+							if tt.expectedControllers[i] == di.Controllers[j] {
+								found = true
+							}
+						}
+						g.Expect(found).To(BeTrue())
+					}
+				}),
+			)
+
+			ctx := context.TODO()
+			err := app.Start(ctx)
+			if !tt.expectedFxError {
+				g.Expect(err).ToNot(HaveOccurred())
+			} else {
+				g.Expect(err).To(HaveOccurred())
+			}
+
+			err = app.Stop(ctx)
+			g.Expect(err).ToNot(HaveOccurred())
+		})
+	}
+}
+
+//TODO: change this to a loop
+func TestValidatingSupportedTarget(t *testing.T) {
+	c := &TestController{}
+	actualType := reflect.TypeOf(c)
+
+	supported := isSupportedType(typeController, actualType)
+
+	if !supported {
+		t.Errorf("expect TestController to be supported because it implements Controller interface")
+	}
+
+	notC := &NotController{}
+	actualType = reflect.TypeOf(notC)
+
+	supported = isSupportedType(typeController, actualType)
+
+	if supported {
+		t.Errorf("expect NotController to not be supported because it does not implement Controller interface")
+	}
+
+	var iface Controller
+	iface = &TestController{}
+	actualType = reflect.TypeOf(iface)
+
+	supported = isSupportedType(typeController, actualType)
+	if !supported {
+		t.Errorf("expect controller to be supported because it implement Controller interface")
+	}
+
+	err := errors.New("some error")
+	actualType = reflect.TypeOf(err)
+
+	supported = isSupportedType(typeError, actualType)
+	if !supported {
+		t.Errorf("expect err to be supported because it implement error interface")
 	}
 }
