661c3fd335daad1cb86e8a5b4709a6eeac87d14c
Livan Du <livdu@cisco.com>
2022-10-28 16:41:01 -0400

    added missing files

---
+++ test/ittest/examples/example_service.go
@@ -0,0 +1,97 @@
+package examples
+
+import (
+	"context"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/integrate/httpclient"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/integrate/security/scope"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/security/access"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/web"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/web/rest"
+	"net/http"
+)
+
+type ExampleRequest struct {
+	UseSystemAccount bool   `json:"sysAcct" form:"sys_acct"`
+	Username         string `json:"user" form:"user"`
+}
+
+type ExampleController struct {
+	Service *ExampleService
+}
+
+func NewExampleController(svc *ExampleService) web.Controller {
+	return &ExampleController{
+		Service: svc,
+	}
+}
+
+func (c *ExampleController) Mappings() []web.Mapping {
+	return []web.Mapping{
+		rest.Get("/remote").Condition(access.RequirePermissions("DUMMY_PERMISSION")).EndpointFunc(c.Remote).Build(),
+		rest.Post("/remote").Condition(access.RequirePermissions("DUMMY_PERMISSION")).EndpointFunc(c.Remote).Build(),
+	}
+}
+
+func (c *ExampleController) Remote(ctx context.Context, req *ExampleRequest) (interface{}, error) {
+	switch {
+	case len(req.Username) == 0:
+		return c.Service.CallRemoteWithCurrentContext(ctx)
+	case req.UseSystemAccount:
+		return c.Service.CallRemoteWithSystemAccount(ctx, req.Username)
+	default:
+		return c.Service.CallRemoteWithoutSystemAccount(ctx, req.Username)
+	}
+}
+
+type ExampleService struct {
+	HttpClient httpclient.Client
+}
+
+func NewExampleService(client httpclient.Client) (*ExampleService, error) {
+	client, e := client.WithService("usermanagementgoservice")
+	if e != nil {
+		return nil, e
+	}
+	return &ExampleService{
+		HttpClient: client,
+	}, nil
+}
+
+// CallRemoteWithSystemAccount switch to given username using system account and make remote HTTP call
+func (s *ExampleService) CallRemoteWithSystemAccount(ctx context.Context, username string) (ret interface{}, err error) {
+	e := scope.Do(ctx, func(ctx context.Context) {
+		ret, err = s.performRemoteHttpCall(ctx)
+	}, scope.UseSystemAccount(), scope.WithUsername(username))
+
+	if e != nil {
+		return nil, e
+	}
+	return
+}
+
+// CallRemoteWithoutSystemAccount switch to given username directly and make remote HTTP call
+func (s *ExampleService) CallRemoteWithoutSystemAccount(ctx context.Context, username string) (ret interface{}, err error) {
+	e := scope.Do(ctx, func(ctx context.Context) {
+		ret, err = s.performRemoteHttpCall(ctx)
+	}, scope.WithUsername(username))
+
+	if e != nil {
+		return nil, e
+	}
+	return
+}
+
+// CallRemoteWithCurrentContext make remote HTTP call using current security context
+func (s *ExampleService) CallRemoteWithCurrentContext(ctx context.Context) (ret interface{}, err error) {
+	return s.performRemoteHttpCall(ctx)
+}
+
+func (s *ExampleService) performRemoteHttpCall(ctx context.Context) (ret interface{}, err error) {
+	resp, e := s.HttpClient.Execute(ctx, httpclient.NewRequest("/api/v8/users/current", http.MethodGet))
+	if e != nil {
+		err = e
+		return
+	}
+	ret = resp.Body
+	return
+}
---
+++ test/ittest/examples/example_test.go
@@ -0,0 +1,230 @@
+package examples
+
+import (
+	"context"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/integrate/httpclient"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/integrate/security/seclient"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/utils"
+	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/web"
+	"cto-github.cisco.com/NFV-BU/go-lanai/test"
+	"cto-github.cisco.com/NFV-BU/go-lanai/test/apptest"
+	"cto-github.cisco.com/NFV-BU/go-lanai/test/ittest"
+	"cto-github.cisco.com/NFV-BU/go-lanai/test/sdtest"
+	"cto-github.cisco.com/NFV-BU/go-lanai/test/sectest"
+	"cto-github.cisco.com/NFV-BU/go-lanai/test/webtest"
+	"encoding/json"
+	"github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
+	"go.uber.org/fx"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+/*************************
+	Tests
+ *************************/
+
+// TestMain globally enable HTTP recording mode
+// Important: this should always be disabled when checking in
+//func TestMain(m *testing.M) {
+//	suitetest.RunTests(m,
+//		ittest.PackageHttpRecordingMode(),
+//	)
+//}
+
+type TestDI struct {
+	fx.In
+	Service    *ExampleService
+	AuthClient seclient.AuthenticationClient
+}
+
+func TestHttpClientWithSecurity(t *testing.T) {
+	var di TestDI
+	test.RunTest(context.Background(), t,
+		apptest.Bootstrap(),
+		// Setup your test, includes modules you need, provide test subjects and other mocks
+		webtest.WithMockedServer(),
+		apptest.WithModules(httpclient.Module),
+		apptest.WithDI(&di),
+		apptest.WithFxOptions(
+			web.FxControllerProviders(NewExampleController),
+			fx.Provide(NewExampleService),
+		),
+
+		// Tell test framework to use recorded HTTP interaction.
+		// Note: this function accept may options. See ittest/httpvcr.go for more details
+		ittest.WithHttpPlayback(t),
+
+		// Tell test framework to use real service for any HTTP interaction.
+		// This should be enabled during development and turned off before checking in the code
+		//ittest.WithHttpPlayback(t, ittest.HttpRecordingMode()),
+
+		// Because the test subjects (ExampleController, ExampleService) uses service discovery and scopes,
+		// They need to be configured properly for HTTP recorder to work
+		ittest.WithRecordedScopes(),
+		sdtest.WithMockedSD(sdtest.DefinitionWithPrefix("mocks.sd")),
+
+		// Because remote access require current context to be authentic during recording mode,
+		// We need this configuration to pass along the security context from the test context to Controller's context
+		sectest.WithMockedMiddleware(),
+
+		// Controller requires permissions, we need to mock it for any test that uses webtest
+		test.SubTestSetup(MockPermissions()),
+
+		// Test order is important, unless ittest.DisableHttpRecordOrdering option is used
+		test.GomegaSubTest(SubTestMockedServerWithSystemAccount(), "TestMockedServerWithSystemAccount"),
+		test.GomegaSubTest(SubTestUnitTestWithSystemAccount(&di), "TestUnitTestWithSystemAccount"),
+		test.GomegaSubTest(SubTestMockedServerWithoutSystemAccount(&di), "TestMockedServerWithoutSystemAccount"),
+		test.GomegaSubTest(SubTestUnitTestWithWithoutSystemAccount(&di), "TestUnitTestWithWithoutSystemAccount"),
+		test.GomegaSubTest(SubTestMockedServerWithCurrentContext(&di), "TestMockedServerWithCurrentContext"),
+		test.GomegaSubTest(SubTestUnitTestWithCurrentContext(&di), "TestUnitTestWithCurrentContext"),
+	)
+}
+
+/*************************
+	Sub Tests
+ *************************/
+
+// A permission setup
+func MockPermissions() test.SetupFunc {
+	return func(ctx context.Context, t *testing.T) (context.Context, error) {
+		// Authentication is passed along to the controller in MockedServer mode.
+		return sectest.ContextWithSecurity(ctx, sectest.MockedAuthentication(func(d *sectest.SecurityDetailsMock) {
+			d.Permissions = utils.NewStringSet("DUMMY_PERMISSION")
+		})), nil
+	}
+}
+
+func SubTestMockedServerWithSystemAccount() test.GomegaSubTestFunc {
+	return func(ctx context.Context, t *testing.T, g *gomega.WithT) {
+		req := webtest.NewRequest(ctx, http.MethodGet, "/remote", nil,
+			webtest.Queries("sys_acct", "true"),
+			webtest.Queries("user", "admin"),
+		)
+		resp := webtest.MustExec(ctx, req).Response
+		assertHttpResponse(t, g, resp, "admin")
+	}
+}
+
+func SubTestUnitTestWithSystemAccount(di *TestDI) test.GomegaSubTestFunc {
+	return func(ctx context.Context, t *testing.T, g *gomega.WithT) {
+		ret, e := di.Service.CallRemoteWithSystemAccount(ctx, "admin")
+		g.Expect(e).To(Succeed(), "functions that calling remote service should not fail")
+		assertResult(t, g, ret, "admin")
+	}
+}
+
+// SubTestMockedServerWithoutSystemAccount
+// Because the test subject (the controller) uses current context,
+// a real access token should be used in HTTP recording mode. (It doesn't matter in replay/playback mode).
+// Therefore, we need additional setup to inject real access token into context.
+// How real access token is obtained is irrelevant:
+// 1. seclient.AuthenticationClient can be used if this user have password
+// 2. A hard corded token also works, as long as it's valid at the time of recording
+func SubTestMockedServerWithoutSystemAccount(di *TestDI) test.GomegaSubTestFunc {
+	return func(ctx context.Context, t *testing.T, g *gomega.WithT) {
+		// get a real token, this can be put in SubTest setup phase
+		rs, e := di.AuthClient.PasswordLogin(ctx, seclient.WithCredentials("superuser", "superuser"))
+		g.Expect(e).To(Succeed(), "initial access token request should be success")
+
+		// Authentication is passed along to the controller in MockedServer mode.
+		// Note that this would override mocked security context in setup func, so we need to re-do it
+		ctx = sectest.ContextWithSecurity(ctx, sectest.MockedAuthentication(func(d *sectest.SecurityDetailsMock) {
+			d.AccessToken = rs.Token.Value()
+			d.Permissions = utils.NewStringSet("DUMMY_PERMISSION")
+		}))
+
+		// Do test as usual
+		req := webtest.NewRequest(ctx, http.MethodPost, "/remote", strings.NewReader(`{"user": "admin"}`),
+			webtest.ContentType("application/json"),
+		)
+		resp := webtest.MustExec(ctx, req).Response
+		assertHttpResponse(t, g, resp, "admin")
+	}
+}
+
+// SubTestUnitTestWithWithoutSystemAccount
+// Calling service directly also requires real access token to work, so same setup
+// as SubTestMockedServerWithoutSystemAccount except that permission mocking is not required
+func SubTestUnitTestWithWithoutSystemAccount(di *TestDI) test.GomegaSubTestFunc {
+	return func(ctx context.Context, t *testing.T, g *gomega.WithT) {
+		// get a real token, this can be put in SubTest setup phase
+		rs, e := di.AuthClient.PasswordLogin(ctx, seclient.WithCredentials("superuser", "superuser"))
+		g.Expect(e).To(Succeed(), "initial access token request should be success")
+
+		// Authentication is passed along to the controller in MockedServer mode.
+		ctx = sectest.ContextWithSecurity(ctx, sectest.MockedAuthentication(func(d *sectest.SecurityDetailsMock) {
+			d.AccessToken = rs.Token.Value()
+		}))
+
+		// Do test as usual
+		ret, e := di.Service.CallRemoteWithoutSystemAccount(ctx, "admin")
+		g.Expect(e).To(Succeed(), "functions that calling remote service should not fail")
+		assertResult(t, g, ret, "admin")
+	}
+}
+
+// SubTestMockedServerWithCurrentContext
+// Because current context is used as-is in service, same configuration as SubTestMockedServerWithoutSystemAccount
+func SubTestMockedServerWithCurrentContext(di *TestDI) test.GomegaSubTestFunc {
+	return func(ctx context.Context, t *testing.T, g *gomega.WithT) {
+		// get a real token, this can be put in SubTest setup phase
+		rs, e := di.AuthClient.PasswordLogin(ctx, seclient.WithCredentials("superuser", "superuser"))
+		g.Expect(e).To(Succeed(), "initial access token request should be success")
+
+		// Authentication is passed along to the controller in MockedServer mode.
+		// Note that this would override mocked security context in setup func, so we need to re-do it
+		ctx = sectest.ContextWithSecurity(ctx, sectest.MockedAuthentication(func(d *sectest.SecurityDetailsMock) {
+			d.AccessToken = rs.Token.Value()
+			d.Permissions = utils.NewStringSet("DUMMY_PERMISSION")
+		}))
+
+		// Do test as usual
+		req := webtest.NewRequest(ctx, http.MethodGet, "/remote", nil)
+		resp := webtest.MustExec(ctx, req).Response
+		assertHttpResponse(t, g, resp, "superuser")
+	}
+}
+
+// SubTestUnitTestWithCurrentContext
+// Calling service directly also requires real access token to work, so same setup
+// as SubTestMockedServerWithCurrentContext except that permission mocking is not required
+func SubTestUnitTestWithCurrentContext(di *TestDI) test.GomegaSubTestFunc {
+	return func(ctx context.Context, t *testing.T, g *gomega.WithT) {
+		// get a real token, this can be put in SubTest setup phase
+		rs, e := di.AuthClient.PasswordLogin(ctx, seclient.WithCredentials("superuser", "superuser"))
+		g.Expect(e).To(Succeed(), "initial access token request should be success")
+
+		// Authentication is passed along to the controller in MockedServer mode.
+		ctx = sectest.ContextWithSecurity(ctx, sectest.MockedAuthentication(func(d *sectest.SecurityDetailsMock) {
+			d.AccessToken = rs.Token.Value()
+		}))
+
+		// Do test as usual
+		ret, e := di.Service.CallRemoteWithCurrentContext(ctx)
+		g.Expect(e).To(Succeed(), "functions that calling remote service should not fail")
+		assertResult(t, g, ret, "superuser")
+	}
+}
+
+/*************************
+	internal
+ *************************/
+
+func assertHttpResponse(t *testing.T, g *gomega.WithT, resp *http.Response, expectedUser string) {
+	g.Expect(resp).To(Not(BeNil()), "response cannot be nil")
+	g.Expect(resp.StatusCode).To(Equal(http.StatusOK), "response should be 200")
+
+	var body map[string]interface{}
+	e := json.NewDecoder(resp.Body).Decode(&body)
+	g.Expect(e).To(Succeed(), "response body should be JSON")
+	assertResult(t, g, body, expectedUser)
+}
+
+func assertResult(_ *testing.T, g *gomega.WithT, i interface{}, expectedUser string) {
+	g.Expect(i).To(Not(BeNil()), "functions that calling remote service should have proper response")
+	g.Expect(i).To(BeAssignableToTypeOf(map[string]interface{}{}), "service should return a map")
+	m := i.(map[string]interface{})
+	g.Expect(m).To(HaveKeyWithValue("username", expectedUser), "body should contains correct username")
+}
