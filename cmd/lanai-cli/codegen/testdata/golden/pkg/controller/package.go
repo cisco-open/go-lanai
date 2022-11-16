a0ac90d8e16f4453e98a797bfc809eb4cd639889
Bill Luu <biluu@cisco.com>
2022-11-16 16:29:59 -0700

    Generate go-lanai controller & service files
    PLAT-1081
    
    Generates controller & service files - currently missing content for the mappings functions & functions the controller will use - will be added in the future.

--- cmd/lanai-cli/codegen/testdata/golden/pkg/controller/package.go
+++ cmd/lanai-cli/codegen/testdata/golden/pkg/controller/package.go
@@ -4,11 +4,17 @@ import (
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/bootstrap"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/utils/validation"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/web"
+	controllerv1 "cto-github.cisco.com/NFV-BU/test-service/pkg/controller/v1"
+	controllerv2 "cto-github.cisco.com/NFV-BU/test-service/pkg/controller/v2"
+	controllerv3 "cto-github.cisco.com/NFV-BU/test-service/pkg/controller/v3"
 	"github.com/go-playground/validator/v10/non-standard/validators"
 	"go.uber.org/fx"
 )
 
 func Use() {
+	controllerv1.Use()
+	controllerv2.Use()
+	controllerv3.Use()
 	bootstrap.AddOptions(
 		fx.Invoke(register),
 	)
