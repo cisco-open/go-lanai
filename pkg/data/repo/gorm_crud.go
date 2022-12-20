453e6144eade0382a8ce27e953817cc585d9f945
Richard Hou <rihou@cisco.com>
2022-12-20 09:36:12 -0500

    PLAT-1271: Golang Gorm Framework - when calling FindById and the id value is a string, the string is treated

--- pkg/data/repo/gorm_crud.go
+++ pkg/data/repo/gorm_crud.go
@@ -5,7 +5,6 @@ import (
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/utils"
 	"cto-github.cisco.com/NFV-BU/go-lanai/pkg/utils/order"
 	"fmt"
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 	"reflect"
@@ -76,20 +75,10 @@ func (g GormCrud) FindById(ctx context.Context, dest interface{}, id interface{}
 			WithMessage(errTmplInvalidCrudValue, dest, "FindById", "*Struct")
 	}
 
-	// TODO verify this using index key
-	switch v := id.(type) {
-	case string:
-		if uid, e := uuid.Parse(v); e == nil {
-			id = uid
-		}
-	case *string:
-		if uid, e := uuid.Parse(*v); e == nil {
-			id = uid
-		}
-	}
+	eq := clause.Eq{Column: clause.PrimaryColumn, Value: id}
 
 	return execute(ctx, g.GormApi.DB(ctx), nil, options, modelFunc(g.model), func(db *gorm.DB) *gorm.DB {
-		return db.Take(dest, id)
+		return db.Take(dest, eq)
 	})
 }
 
