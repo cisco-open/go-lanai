4e1613452259e12221dcc2bf6adb09a78a84f173
Bill Luu <biluu@cisco.com>
2023-02-14 20:48:21 -0700

    Change option field validation to always have omitempty if exists

--- cmd/lanai-cli/codegen/generator/internal/structtags.go
+++ cmd/lanai-cli/codegen/generator/internal/structtags.go
@@ -45,10 +45,10 @@ func nameType(element interface{}) string {
 
 func bindings(propertyName string, element *openapi3.SchemaRef, requiredList []string) string {
 	var bindingParts []string
-
-	bindingParts = append(bindingParts, omitEmptyTags(propertyName, requiredList, element)...)
+	validationTags := validationTags(element, requiredList)
+	bindingParts = append(bindingParts, omitEmptyTags(propertyName, requiredList, len(validationTags), element)...)
 	bindingParts = append(bindingParts, requiredTag(propertyName, requiredList)...)
-	bindingParts = append(bindingParts, validationTags(element, requiredList)...)
+	bindingParts = append(bindingParts, validationTags...)
 	return strings.Join(bindingParts, ",")
 }
 
@@ -99,12 +99,12 @@ func limitTags(element *openapi3.SchemaRef) (result []string) {
 	return result
 }
 
-func omitEmptyTags(propertyName string, requiredList []string, schemaRef *openapi3.SchemaRef) (result []string) {
+func omitEmptyTags(propertyName string, requiredList []string, numberOfValidationTags int, schemaRef *openapi3.SchemaRef) (result []string) {
 	if schemaRef == nil {
 		return result
 	}
 
-	if !listContains(requiredList, propertyName) && valuePassesValidation(schemaRef.Value, zeroValue(schemaRef.Value)) {
+	if !listContains(requiredList, propertyName) && numberOfValidationTags > 0 {
 		result = append(result, "omitempty")
 	}
 	return result
