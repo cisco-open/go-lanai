a68056c9052e3815cbb9a7d78c0e9b5a495c7b66
Byron Seto <bseto@cisco.com>
2023-05-31 13:44:10 -0600

    re-records tests and creates a commented out test

--- test/ittest/examples/example_test.go
+++ test/ittest/examples/example_test.go
@@ -11,7 +11,6 @@ import (
 	"cto-github.cisco.com/NFV-BU/go-lanai/test/ittest"
 	"cto-github.cisco.com/NFV-BU/go-lanai/test/sdtest"
 	"cto-github.cisco.com/NFV-BU/go-lanai/test/sectest"
-	"cto-github.cisco.com/NFV-BU/go-lanai/test/suitetest"
 	"cto-github.cisco.com/NFV-BU/go-lanai/test/webtest"
 	"encoding/json"
 	"github.com/onsi/gomega"
@@ -29,11 +28,11 @@ import (
 
 // TestMain globally enable HTTP recording mode
 // Important: this should always be disabled when checking in
-func TestMain(m *testing.M) {
-	suitetest.RunTests(m,
-		ittest.PackageHttpRecordingMode(),
-	)
-}
+//func TestMain(m *testing.M) {
+//	suitetest.RunTests(m,
+//		ittest.PackageHttpRecordingMode(),
+//	)
+//}
 
 type TestDI struct {
 	fx.In
