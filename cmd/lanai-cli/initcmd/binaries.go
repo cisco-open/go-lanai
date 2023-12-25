5e5792a74c987d488f3a50362f3da906e5dc93c3
Livan Du <livdu@cisco.com>
2023-12-25 15:47:34 -0500

    removed private repo dependencies during make init

--- cmd/lanai-cli/initcmd/binaries.go
+++ cmd/lanai-cli/initcmd/binaries.go
@@ -17,43 +17,42 @@
 package initcmd
 
 import (
-	"context"
-	"fmt"
-	"os"
+    "context"
+    "fmt"
+    "os"
 
-	"cto-github.cisco.com/NFV-BU/go-lanai/cmd/lanai-cli/cmdutils"
+    "cto-github.cisco.com/NFV-BU/go-lanai/cmd/lanai-cli/cmdutils"
 )
 
 var defaultBinaries = map[string]string{
-	"github.com/axw/gocov/gocov":                            "v1.1.0",
-	"github.com/AlekSi/gocov-xml":                           "v1.0.0",
-	"gotest.tools/gotestsum":                                "v1.8.0",
-	"github.com/golangci/golangci-lint/cmd/golangci-lint":   "v1.53.1",
-	"github.com/jstemmer/go-junit-report":                   "v0.9.1",
-	"cto-github.cisco.com/NFV-BU/contract-verifier/cli/ccv": "v1.4.2",
+    "github.com/axw/gocov/gocov":                          "v1.1.0",
+    "github.com/AlekSi/gocov-xml":                         "v1.0.0",
+    "gotest.tools/gotestsum":                              "v1.8.0",
+    "github.com/golangci/golangci-lint/cmd/golangci-lint": "v1.53.1",
+    "github.com/jstemmer/go-junit-report":                 "v0.9.1",
 }
 
 func installBinaries(ctx context.Context) error {
-	opts := []cmdutils.ShCmdOptions{cmdutils.ShellShowCmd(true), cmdutils.ShellUseWorkingDir(), cmdutils.ShellStdOut(os.Stdout)}
+    opts := []cmdutils.ShCmdOptions{cmdutils.ShellShowCmd(true), cmdutils.ShellUseWorkingDir(), cmdutils.ShellStdOut(os.Stdout)}
 
-	binaries := make(map[string]string)
+    binaries := make(map[string]string)
 
-	for p, v := range defaultBinaries {
-		binaries[p] = v
-	}
-	for _, b := range Module.Binaries {
-		if b.Package == "" || b.Version == "" {
-			logger.Warnf(`Invalid binaries entry in Module.yml: package="%s", version="%s"`, b.Package, b.Version)
-			continue
-		}
-		binaries[b.Package] = b.Version
-	}
+    for p, v := range defaultBinaries {
+        binaries[p] = v
+    }
+    for _, b := range Module.Binaries {
+        if b.Package == "" || b.Version == "" {
+            logger.Warnf(`Invalid binaries entry in Module.yml: package="%s", version="%s"`, b.Package, b.Version)
+            continue
+        }
+        binaries[b.Package] = b.Version
+    }
 
-	for p, v := range binaries {
-		installCmd := fmt.Sprintf("go install %s@%s", p, v)
-		opts = append(opts, cmdutils.ShellCmd(installCmd))
-	}
+    for p, v := range binaries {
+        installCmd := fmt.Sprintf("go install %s@%s", p, v)
+        opts = append(opts, cmdutils.ShellCmd(installCmd))
+    }
 
-	_, e := cmdutils.RunShellCommands(ctx, opts...)
-	return e
+    _, e := cmdutils.RunShellCommands(ctx, opts...)
+    return e
 }
