f963d3b37660ccfb282d417d539c2b3cadf1e34e
Livan Du <livdu@cisco.com>
2024-01-30 07:39:16 -0500

    more changes for pr verify test

--- pkg/utils/new_utils.go
+++ pkg/utils/new_utils.go
@@ -1,10 +1,19 @@
 package utils
 
-import "fmt"
+import (
+	errorutils "cto-github.cisco.com/NFV-BU/go-lanai/pkg/utils/error"
+	"fmt"
+	"strconv"
+)
 
 func NewFunc() error {
 	for i := 0; i < 10; i++ {
 		fmt.Printf("Dummy function %d", i)
+	}
+	// this would cause lint issues
+	switch _, e := strconv.ParseBool("true"); e.(type) {
+	case errorutils.CodedError:
+		return nil
 	}
 	return nil
 }
