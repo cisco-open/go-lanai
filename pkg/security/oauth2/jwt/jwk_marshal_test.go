e63cbd7e41299fe64d71d0a592d8442c2234ea82
Livan Du <livdu@cisco.com>
2024-06-11 17:40:17 -0400

    fixed jwk marshaling test

--- pkg/security/oauth2/jwt/jwk_marshal_test.go
+++ pkg/security/oauth2/jwt/jwk_marshal_test.go
@@ -69,9 +69,9 @@ var (
 
 const (
 	TokenES256 = `eyJhbGciOiJFUzI1NiIsImtpZCI6ImR1bW15LWtpZCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0ZXN0ZXIiLCJjbGllbnRfaWQiOiJkdW1teS1jbGllbnQiLCJpYXQiOjE3MTY1NzYwMDMsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODkwMC9hdXRoIiwianRpIjoiZTA3ZGY5NTUtMjhmNC00ZmUwLThiZmQtNTRkNGRmNGE2NTliIiwibmJmIjoxNzE2NTc2MDAzLCJzdWIiOiJteS11c2VyIn0.vkbw2SViTkLFRI8H4kF12qNNggXkHumSOoHWldg2tAU2-yl9iTDR8JZc-n0yz20Z0tFhp-BDgiNQ68pJxVhdMg`
-	TokenRS256 = `eyJhbGciOiJSUzI1NiIsImtpZCI6ImR1bW15LWtpZCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0ZXN0ZXIiLCJjbGllbnRfaWQiOiJkdW1teS1jbGllbnQiLCJpYXQiOjE3MTY1NzYwMDMsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODkwMC9hdXRoIiwianRpIjoiZTA3ZGY5NTUtMjhmNC00ZmUwLThiZmQtNTRkNGRmNGE2NTliIiwibmJmIjoxNzE2NTc2MDAzLCJzdWIiOiJteS11c2VyIn0.Z--67tOijHME_F1UhMLTtTbJ6T30BNY6F-OxDL4N1Sg4_GhffNjONL8Ar8iji_IAds_DC961jYoppX9RF9BDkr33kvZsfXMAedBGjccRRn2o93-4v-OwF3aDgJxqffjhb9M2NGr5I7MLPwAiwaqW8-IkkwOeoBTwCQa7BQ5nxeptXGQ8gT0vKMAj6GnVH-2QVpBFVM5CXbJDQx7LNOzKpMYxW5LTQmrJlZamtSxyvYsp4pMh1hSe56XcPEFbKQ84ugHk5YaedGzmSAkwxv_vz-_X1Hk2T7S2aDYZ9Pey2FK8jxYOTq9vGKpQX6zggf0Qz4y7FOpMWZ8Xzw851z2S8A`
-	TokenHS256 = `eyJhbGciOiJIUzI1NiIsImtpZCI6ImR1bW15LWtpZCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0ZXN0ZXIiLCJjbGllbnRfaWQiOiJkdW1teS1jbGllbnQiLCJpYXQiOjE3MTY1NzYwMDMsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODkwMC9hdXRoIiwianRpIjoiZTA3ZGY5NTUtMjhmNC00ZmUwLThiZmQtNTRkNGRmNGE2NTliIiwibmJmIjoxNzE2NTc2MDAzLCJzdWIiOiJteS11c2VyIn0.jwHlGBiJC_8LbA-KAfaIodb6spn_DjYb8AvAFDU53eI`
-	TokenEdDSA = ``
+	TokenRS256 = `eyJhbGciOiJSUzI1NiIsImtpZCI6ImR1bW15LWtpZCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0ZXN0ZXIiLCJjbGllbnRfaWQiOiJkdW1teS1jbGllbnQiLCJpYXQiOjE3MTY1NzYwMDMsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODkwMC9hdXRoIiwianRpIjoiZTA3ZGY5NTUtMjhmNC00ZmUwLThiZmQtNTRkNGRmNGE2NTliIiwibmJmIjoxNzE2NTc2MDAzLCJzdWIiOiJteS11c2VyIn0.dr-vcQFByI4L8pDq6IVbOkBG28GOlnzYU5oPmxskGYZFd4MQT4OVcEUQEoe-JfstcvnhHY6JhJO0VgmwWjDRc5LvXYYFIlamyaNecxqXjDNy0JunILLIpQoQSZxft_gHIkOx0swe6pwhHfpRo6QEZ2sPMeH7ECJPrpgrdF1xfLKJvucbGvl7ArO-hrn4YvJbYUetki4IB9kGj33deYsuzNuUi-4JMrLhCBsqcrmRko2pWnTyX48m0GZ0nwLzuO4C5tLOjadZGROX2BZr6ozr7tmQJ8umLvUlhfBlR6ZV4CY2RTz4JYD8ZiJDqMzoS86SREu-7plBmZ3sRvHItfkwqA`
+	TokenHS256 = `eyJhbGciOiJIUzI1NiIsImtpZCI6ImR1bW15LWtpZCIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0ZXN0ZXIiLCJjbGllbnRfaWQiOiJkdW1teS1jbGllbnQiLCJpYXQiOjE3MTY1NzYwMDMsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODkwMC9hdXRoIiwianRpIjoiZTA3ZGY5NTUtMjhmNC00ZmUwLThiZmQtNTRkNGRmNGE2NTliIiwibmJmIjoxNzE2NTc2MDAzLCJzdWIiOiJteS11c2VyIn0.3YckAp_uQK4FUQqXQLxbEmvvihBLowrftXGjdl5Ybf4`
+	TokenEdDSA = `eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidGFyZ2V0Il0sImV4cCI6MTcxODIyODI5OCwiaWF0IjoxNzE4MTQxODk4LCJpc3MiOiJzYW5kYm94IiwianRpIjoiYjhkMDAyN2UtMDJlOC00MDM1LWIwNzctNTlmNDkxY2QwOGM0IiwibmJmIjoxNzE4MTQxODk4LCJzdWIiOiJ1c2VyIn0.3FXSxCXwXiZxZpj1cVmt1_ZvuU5N91tH5PYd1kRFEK-pKVpi2JxVnPeK1oEA7kIztAaBA1REzCpZviLlkzWWBg`
 )
 
 /*************************
@@ -100,7 +100,7 @@ func TestJwkUnmarshalling(t *testing.T) {
 		test.GomegaSubTest(SubTestJwkUnmarshal(jwt.SigningMethodRS256, TokenRS256, ExpectRSA), "RSA-RS256"),
 		test.GomegaSubTest(SubTestJwkUnmarshal(jwt.SigningMethodES256, TokenES256, ExpectEC256), "EC-ES256"),
 		test.GomegaSubTest(SubTestJwkUnmarshal(jwt.SigningMethodHS256, TokenHS256, ExpectOct), "HMAC-HS256"),
-		//test.GomegaSubTest(SubTestJwkUnmarshal(jwt.SigningMethodEdDSA, TokenEdDSA, ExpectOKP), "Ed25519-EdDSA"),
+		test.GomegaSubTest(SubTestJwkUnmarshal(jwt.SigningMethodEdDSA, TokenEdDSA, ExpectOKP), "Ed25519-EdDSA"),
 	)
 }
 
@@ -185,7 +185,7 @@ func AssertJwkPair(ctx context.Context, g *gomega.WithT, pub Jwk, priv PrivateJw
 
 func AssertJwt(ctx context.Context, g *gomega.WithT, pub Jwk, token string) {
 	pubStore := &TestJwkStore{Jwk: pub}
-	decoder := NewSignedJwtDecoder(VerifyWithJwkStore(pubStore, TestDummyKid))
+	decoder := NewSignedJwtDecoder(VerifyWithJwkStore(pubStore, TestDummyKid), VerifyWithMethods(SupportedSigningMethods...))
 	decoded, e := decoder.Decode(ctx, token)
 	g.Expect(e).To(Succeed(), "decode JWT with public JWK should not fail")
 	g.Expect(decoded).ToNot(BeZero(), "decoded claims with public JWK should not be zero")
